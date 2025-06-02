a5434f5e0cfd43d6164d262a066d0118c7fe283b
fakhraldeen alfallatah <fakhraldeen18@gmail.com>
2025-06-02 20:45:14 +0300

    fix project issue

--- src/Controllers/ProjectsController.cs
+++ src/Controllers/ProjectsController.cs
@@ -133,6 +133,7 @@ public class ProjectsController : CustomController
     [HttpGet("NumberOfProjects")]
     [ProducesResponseType(StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [Authorize(Roles = "TeamMember,ProjectManager,Admin")]
     public async Task<ActionResult> NumberOfProject([FromQuery] string? status)
     {
         var userId = GetUserIdFromToken();
